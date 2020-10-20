eff97323c22085920c1bd50a886becb68747ea7f
Javier García <javigdeveloper@gmail.com>
2020-10-19 23:41:15 -0500

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./components/TodoList";
 import uuidv4 from "uuid/dist/v4";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value;
@@ -14,13 +32,19 @@ function App() {
     });
     todoNameRef.current.value = null;
   }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter((todo) => !todo.complete);
+    setTodos(newTodos);
+  }
+
   return (
     <div>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text"></input>
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed Todos</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearTodos}>Clear Completed</button>
+      <p>{todos.filter((todo) => !todo.complete).length} left to do</p>
     </div>
   );
 }
